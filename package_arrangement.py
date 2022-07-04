20ca3cff34d1ac05aea70bd54a75b581e6fc8817
nicholashennen <108638268+nicholashennen@users.noreply.github.com>
2022-07-03 21:02:54 -0500

    Create package_arrangement.py

---
+++ package_arrangement.py
@@ -0,0 +1,13 @@
+# Libraries in standard distubtion of Python yet require being imported
+# for use as opposed to built-in fuctions and types for use are listed
+# first. All libaries and packages are listed in alphabetical order.
+import datetime
+import os.path
+import pprint
+import random
+import shutil
+
+# Third party packages are separated standard Python libraries by one line
+import ibm_db
+import pandas
+import requests
