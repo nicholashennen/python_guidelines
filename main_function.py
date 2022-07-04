70ed976cd04778ed7f80701e57e6d583e9e6af03
nicholashennen <108638268+nicholashennen@users.noreply.github.com>
2022-07-04 11:25:47 -0500

    Update main_function.py

--- main_function.py
+++ main_function.py
@@ -1,8 +1,20 @@
-"""Simple Program Start"""
+"""
+Simple Program Start.
+Use checking for if __name__ == "__main__" can help prevent unwanted app
+execution.
+Use function main() as a driver to call other functions and classes.
+"""
 
 
 def main():
     print("function main called.")
+
+    # Call second_function()
+    second_function()
+
+
+def second_function():
+    print("A second function was called from function main.")
 
 
 if __name__ == "__main__":
